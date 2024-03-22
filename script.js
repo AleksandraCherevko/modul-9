018195101c7b075d1db4bde2ad556e6238fcf182
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-03-22 15:42:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const settings = {
-  theme: "dark",
-  isAuthenticated: true,
-  options: [1, 2, 3],
-};
+// const settings = {
+//   theme: "dark",
+//   isAuthenticated: true,
+//   options: [1, 2, 3],
+// };
 
-localStorage.setItem("settings", JSON.stringify(settings));
-const savedsetting = localStorage.getItem("settings");
-console.log(savedsetting);
+// localStorage.setItem("settings", JSON.stringify(settings));
+// const savedsetting = localStorage.getItem("settings");
+// console.log(savedsetting);
 
-const parsedSettings = JSON.parse(savedsetting);
-console.log(parsedSettings);
+// const parsedSettings = JSON.parse(savedsetting);
+// console.log(parsedSettings);
+
